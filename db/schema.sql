e8f3925263751584cc4d86a0a2c2c0f77c869726
Stilan <40999153+Stilan@users.noreply.github.com>
2022-04-10 16:51:38 +0700

    2. Регистрация пользователя [#296069]

--- db/schema.sql
+++ db/schema.sql
@@ -39,18 +39,25 @@ CREATE TABLE  accident_rules (
     rule_id int references rules(id) NOT NULL
 );
 
-CREATE TABLE users (
-  username VARCHAR(50) NOT NULL,
-  password VARCHAR(100) NOT NULL,
-  enabled boolean default true,
-  PRIMARY KEY (username)
+CREATE TABLE authorities (
+  id serial primary key,
+  authority VARCHAR(50) NOT NULL unique
 );
 
-CREATE TABLE authorities (
-  username VARCHAR(50) NOT NULL,
-  authority VARCHAR(50) NOT NULL,
-  FOREIGN KEY (username) REFERENCES users(username)
+CREATE TABLE users (
+  id serial primary key,
+  username VARCHAR(50) NOT NULL unique,
+  password VARCHAR(100) NOT NULL,
+  enabled boolean default true,
+  authority_id int not null references authorities(id)
 );
+
+insert into authorities (authority) values ('ROLE_USER');
+insert into authorities (authority) values ('ROLE_ADMIN');
+
+insert into users (username, enabled, password, authority_id)
+values ('root', true, '$2a$10$SVylsHUwxWCOYi8CCC72gOSu8Fth7FY0TSAXXmiJtLSdECZjsOZ0y',
+(select id from authorities where authority = 'ROLE_ADMIN'));
 
 insert into accident_rules(accident_id, rule_id) VALUES(1, 1);
 select * from accident_rules;
